37c870517f5ba017f0afe743116a46d2dac7344c
Chirag Dodhia <chirag.dodhia@gocomet.in>
2020-07-19 10:06:57 +0530

    removed credentials

--- src/trello.js
+++ src/trello.js
@@ -1,9 +1,8 @@
 const axios = require('axios').default;
 var url = "www.dos.com"
 var card_id = "mXzuCKKO"
-var key = "4533fe0499c529cf52498f57a7bb2cc6"
-var token = "f8d3591096b170bd34510a46cce7b1e17c66cd1a8c16d5808d0c3dd68ad60b3b"
 
+// key and token taken from me later when you are going to change the code
 //send all of the above the above variables as paramaters to this function
 function create_attachment() {
   axios.post('https://api.trello.com/1/cards/'+ card_id + '/attachments', {
